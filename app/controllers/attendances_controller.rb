a83aa5e862cf19aef035ea03244cda1403a5c470
Steven Jack Chung <stevenjchung12@gmail.com>
2020-11-19 12:29:25 -0500

    Finish invite system

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,11 +1,13 @@
 class AttendancesController < ApplicationController
   def create
-    unless User.find(session[:current_user]['id']).attendances.where(event_id: params[:event_id]).any?
-      user = User.find(session[:current_user]['id'])
+
+    puts params[:event_id]
+    unless User.find(params[:user][:user_id]).attendances.where(event_id: params[:event_id]).any?
+      user = User.find(params[:user][:user_id])
       a = Attendance.new(event_id: params[:event_id], user_id: user.id)
+      puts a.event_id, a.user_id
       a.save
     end
-
     redirect_to events_path
   end
 end
