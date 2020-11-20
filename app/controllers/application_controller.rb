1235b907f4f2c0425e208ce2ba76b51b6665d5de
Steven Jack Chung <stevenjchung12@gmail.com>
2020-11-20 12:24:54 -0500

    Hide attend button if already attending

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,8 @@ class ApplicationController < ActionController::Base
     date = DateTime.now
     date.localtime.strftime('%m/%d/%Y %H:%M')
   end
+  helper_method :current_user_now
+  def current_user_now
+    User.find(session[:current_user]['id'])
+  end
 end
