a754d24080df47076432b375d4b3c8f4a92442f4
Steven Jack Chung <stevenjchung12@gmail.com>
2020-11-20 10:44:59 -0500

    Add animation to error message

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -6,17 +6,22 @@ class AttendancesController < ApplicationController
       user = User.find(params[:user][:user_id])
       a = Attendance.new(event_id: params[:event_id], user_id: user.id)
       a.save
-      p params[:user][:user_id]
-      p session[:current_user]['id']
-      if (params[:user][:user_id]).to_i == session[:current_user]['id']
-        msg = "You decided to attend #{Event.find(params[:event_id]).name}"
-      else
-        msg = "You decided to invite #{User.find(params[:user][:user_id]).username} to attend #{Event.find(params[:event_id]).name}"
-      end
+      msg = build_msg
     else
       msg = "You are already attending to #{Event.find(params[:event_id]).name}"
     end
     redirect_to event_path(params[:event_id]), notice: msg
+  end
+
+  private
+
+  def build_msg
+    if (params[:user][:user_id]).to_i == session[:current_user]['id']
+      msg = "You decided to attend #{Event.find(params[:event_id]).name}"
+    else
+      msg = "You decided to invite #{User.find(params[:user][:user_id]).username} to attend #{Event.find(params[:event_id]).name}"
+    end
+    msg
   end
 end
 
